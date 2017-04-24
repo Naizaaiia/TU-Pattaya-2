7ed89f3510c77d9f53dfc2aafd5366b694efd960
Naizaaiia <naizaaiia@gmail.com>
2017-04-24 23:04:15 +0700

    Admin/AutoEN
    PHP

---
+++ AutoEN/EditOneAutoEN.php
@@ -0,0 +1,73 @@
+<?php
+	$idSelect=$_GET['id'];
+	require'../Connect/dbConnect.php';
+	$sql="select * from Autoen where id=$idSelect";
+	$result= mysqli_query($con,$sql);
+	$row=mysqli_fetch_assoc($result);
+	
+?>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>EditForm</title>
+ <link rel="stylesheet" href="../css/admin.css">
+	</head>
+	<body>
+	<ul>
+ 
+  <li><a href="../Login/TableLogin.php">login</a></li>
+  <li><a href="../HomeTH/TableHomeTH.php">HomeTH</a></li>
+  <li><a href="../HomeEN/TableHomeEN.php">HomeEN</a></li>
+  <li><a href="../HistoryTH/TableHistTH.php">HistoryTH</a></li>
+  <li><a href="../HistoryEN/TableHistEN.php">HistoryEN</a></li>
+  <li><a href="../AdmiTH/TableAdmiTH.php">AdminstrativeBoardTH</a></li>
+  <li><a href="../AdmiEN/TableAdmiEN.php">AdminstrativeBoardEN</a></li>
+  <li><a href="../AddrTH/TableAddrTH.php">AddressAndMapTH</a></li>
+  <li><a href="../AddrEN/TableAddrEN.php">AddressAndMapEN</a></li>
+  <li><a href="../StudTH/TableStudTH.php">Student'sLifeTH</a></li>
+  <li><a href="../StudEN/TableStudEN.php">Student'sLifeEN</a></li>
+  <li><a href="../SoftTH/TableSoftTH.php">ProgramSoftTH</a></li>
+  <li><a href="../SoftEN/TableSoftEN.php">ProgramSoftEN</a></li>
+  <li><a href="../AutoTH/TableAutoTH.php">ProgramAutoTH</a></li>
+  <li><a href="../AutoEN/TableAutoEN.php">ProgramAutoEN</a></li>
+  <li><a href="../DepaTH/TableDepaTH.php">DepartmentTH</a></li>
+  <li><a href="../DepaEN/TableDepaEN.php">DepartmentEN</a></li>
+  <li><a href="../NewTH/TableNewTH.php">NewandEventTH</a></li>
+  <li><a href="../NewEN/TableNewEN.php">NewandEventEN</a></li>
+  
+</ul>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+	</head>
+	<body>
+		<form method="post" action='EditTwoAutoEN.php' enctype="multipart/form-data">
+			<input type="hidden" value="<?php echo $row["id"]?>" name='id_select'/>
+			<input type="file" name="image" id="fileToUpload" value='<?php echo $row['Image']?>'><br><br>	
+			Enter Your Header1:<input type="text" name='Header1' value='<?php echo $row['Header1']?>'><br><br>
+			Enter Your Header2:<input type="text" name='Header2' value='<?php echo $row['Header2']?>'><br><br>
+			Enter Your Header3:<input type="text" name='Header3' value='<?php echo $row['Header3']?>'><br><br>
+			Enter Your Header4:<input type="text" name='Header4' value='<?php echo $row['Header4']?>'><br><br>
+			Enter Your Header5:<input type="text" name='Header5' value='<?php echo $row['Header5']?>'><br><br>
+			Enter Your Header6:<input type="text" name='Header6' value='<?php echo $row['Header6']?>'><br><br>
+			Enter Your Detail1:<input type="text" name='Detail1' value='<?php echo $row['Detail1']?>'><br><br>
+			Enter Your Detail2:<input type="text" name='Detail2' value='<?php echo $row['Detail2']?>'><br><br>
+			Enter Your Detail3:<input type="text" name='Detail3' value='<?php echo $row['Detail3']?>'><br><br>
+			Enter Your Detail4:<input type="text" name='Detail4' value='<?php echo $row['Detail4']?>'><br><br>
+			Enter Your Detail5:<input type="text" name='Detail5' value='<?php echo $row['Detail5']?>'><br><br>
+			
+			
+			
+			
+			<br>
+			
+			
+			
+			
+
+			<br><br>
+			
+			<input type="submit" value="Edit">
+		</form>
+		</div>
+	</body>
+</html>
---
+++ AutoEN/EditTwoAutoEN.php
@@ -0,0 +1,28 @@
+<?php
+	$target = "name/".basename($_FILES['image']['name']);
+	require'../Connect/dbConnect.php';//เชื่อมต่อฐานข้อมูล
+	//รับค่าจากFormมาใส่ในฐานข้อมูล
+	$Image = $_FILES['image']['name'];
+	$Header1 =$_POST['Header1'];
+	$Header2 =$_POST['Header2'];
+	$Header3 =$_POST['Header3'];
+	$Header4 =$_POST['Header4'];
+	$Header5 =$_POST['Header5'];
+	$Header6 =$_POST['Header6'];
+	$Detail1 =$_POST['Detail1'];
+	$Detail2 =$_POST['Detail2'];
+	$Detail3 =$_POST['Detail3'];
+	$Detail4 =$_POST['Detail4'];
+	$Detail5 =$_POST['Detail5'];
+
+	$id_select=$_POST['id_select'];
+	$sql="update autoen set Image="."'$Image',Header1='$Header1',Header2='$Header2',Header3='$Header3',Header4='$Header4',Header5='$Header5',Header6='$Header6',Detail1='$Detail1',Detail2='$Detail2',Detail3='$Detail3',Detail4='$Detail4',Detail5='$Detail5' where id=$id_select";
+	$result=mysqli_query($con,$sql);
+	
+	if($result){
+	echo"success";
+	echo "<script> window.location.assign('TableAutoEN.php'); </script>";
+	}else{
+		echo "เกิดข้อผิดพลาด".mysqli_error($con);
+	}
+?>
