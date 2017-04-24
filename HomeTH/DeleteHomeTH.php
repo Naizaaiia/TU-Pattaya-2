d5a63183ae9e4a7fba499adb2e8bdd053aa74247
Naizaaiia <naizaaiia@gmail.com>
2017-04-24 23:08:48 +0700

    Admin/HomeTH
    PHP

---
+++ HomeTH/DeleteHomeTH.php
@@ -0,0 +1,14 @@
+<?php
+	require'../Connect/dbConnect.php';
+	$idSelect=$_GET['id'];
+		$sql="delete from hometh where id =$idSelect";
+		$result=mysqli_query($con,$sql);
+	if($result){
+	echo"success";
+	echo "<script> window.location.assign('TableHomeTH.php'); </script>";
+		
+	}else{
+		echo"เกิดข้อผิดพลาด";
+	}
+
+?>
