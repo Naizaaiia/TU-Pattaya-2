0b7cf69c92794c568d212f6483445268bd7e3349
chanunya3005 <may_funny_lovem@hotmail.com>
2017-04-26 00:38:43 +0700

    Update DeleteAutoEN.php

--- Admin/AutoEN/DeleteAutoEN.php
+++ Admin/AutoEN/DeleteAutoEN.php
@@ -1,11 +1,11 @@
 <?php
 	require'../Connect/dbConnect.php';
 	$idSelect=$_GET['id'];
-		$sql="delete from autoen where id =$idSelect";
+		$sql="delete from autoth where id =$idSelect";
 		$result=mysqli_query($con,$sql);
 	if($result){
 	echo"success";
-	echo "<script> window.location.assign('TableAutoEN.php'); </script>";
+	echo "<script> window.location.assign('TableAutoTH.php'); </script>";
 		
 	}else{
 		echo"เกิดข้อผิดพลาด";
