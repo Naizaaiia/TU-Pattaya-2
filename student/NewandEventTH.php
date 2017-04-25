81881593db38058f7ec5b5ad70e79bcbd11a3fb0
fewtaeyeon <fewtaeyeon@gmail.com>
2017-04-26 02:55:38 +0700

    Update NewandEventTH.php

--- student/NewandEventTH.php
+++ student/NewandEventTH.php
@@ -10,54 +10,54 @@
 
 <body>
 
-<!--manu bar-->
-<div class="tavi">
-  <div class="logoTU">
-    <a href="mainTH.php">
+  <!--manu bar-->
+  <div class="tavi">
+    <div class="logoTU">
+      <a href="mainTH.php">
         <br><img src="image/iconTU.jpg" style="width: 70px;height: 70px;">
       </a>
-    <div class="langTH">
-      <a href="NewandEventTH.php">
-        <img src="image/iconTH2.png" alt="Thailand" style="width: 40px;height: 30px;">
-      </a>
-      <div class="langEN">
-        <a href="NewandEventEN.php">
-          <img src="image/iconEN2.png" alt="Thailand" style="width: 40px;height: 30px;">
-      </a>
+      <div class="langTH">
+        <a href="NewandEventTH.php">
+          <img src="image/iconTH2.png" alt="Thailand" style="width: 40px;height: 30px;">
+        </a>
+        <div class="langEN">
+          <a href="NewandEventEN.php">
+            <img src="image/iconEN2.png" alt="Thailand" style="width: 40px;height: 30px;">
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-  <h6>คณะวิศวกรรมศาสตร์ มหาวิทยาลัยธรรมศาสตร์ ศูนย์พัทยา</h6>
-  <p class="ma">Faculty of Engineering Thammasart University Pattaya Campus</p>
-  
-  <a href="NewandEventTH.php" class="selected">ข่าวสารและกิจกรรม</a>
+    <h6>คณะวิศวกรรมศาสตร์ มหาวิทยาลัยธรรมศาสตร์ ศูนย์พัทยา</h6>
+    <p class="ma">Faculty of Engineering Thammasart University Pattaya Campus</p>
+    
+    <a href="NewandEventTH.php" class="selected">ข่าวสารและกิจกรรม</a>
 
-  <a href="DepartmentTH.php" class="nav">หน่วยงาน</a>
+    <a href="DepartmentTH.php" class="nav">หน่วยงาน</a>
 
-  <li class="dropdown" >
-    <a class="nav" >หลักสูตร</a>
-    <div class="dropdown-content" >
-    <a href="ProgramSoftTH.php" >วิศวกรรมซอฟต์แวร์</a>
-    <a href="ProgramAutoTH.php" >วิศวกรรมยานยนต์</a>
-    </div>
-  </li>
-
-  <a href="Student'sLifeTH.php" class="nav">ชีวิตนักศึกษา</a>
-
-  <li class="dropdown">
-    <a class="nav">เกี่ยวกับคณะ</a>
-    <div class="dropdown-content" >
-    <a href="HistoryTH.php" >ประวัติ</a>
-    <a href="AdminstrativeBoardTH.php" >ผู้บริหารคณะ</a>
-    <a href="AddressAndMapTH.php" >ที่อยู่เเละแผนที่</a>
-    </div>
+    <li class="dropdown" >
+      <a class="nav" >หลักสูตร</a>
+      <div class="dropdown-content" >
+        <a href="ProgramSoftTH.php" >วิศวกรรมซอฟต์แวร์</a>
+        <a href="ProgramAutoTH.php" >วิศวกรรมยานยนต์</a>
+      </div>
     </li>
 
-  <a href="mainTH.php" class="nav">หน้าหลัก</a>
- </div>
-<!--End manu bar-->
+    <a href="Student'sLifeTH.php" class="nav">ชีวิตนักศึกษา</a>
 
-<!--Slider-->
+    <li class="dropdown">
+      <a class="nav">เกี่ยวกับคณะ</a>
+      <div class="dropdown-content" >
+        <a href="HistoryTH.php" >ประวัติ</a>
+        <a href="AdminstrativeBoardTH.php" >ผู้บริหารคณะ</a>
+        <a href="AddressAndMapTH.php" >ที่อยู่เเละแผนที่</a>
+      </div>
+    </li>
+
+    <a href="mainTH.php" class="nav">หน้าหลัก</a>
+  </div>
+  <!--End manu bar-->
+
+  <!--Slider-->
   <div class="parent">
     <div class="slider">
       <ul class="slider-ul">
@@ -85,45 +85,47 @@
   <br><br><br>
 
   <h1>
-       &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; ข่าวสารและกิจกรรม
-      </h1>
-  <br><br><br>
+   &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; ข่าวสารและกิจกรรม
+ </h1>
+ <br><br><br>
 
-  <?php
-	
-	$db = mysqli_connect("localhost","root","","cn342");
-				$sql = "SELECT *FROM newth ORDER BY id DESC";
-				$result = mysqli_query($db, $sql);
-				
-				while($row = mysqli_fetch_array($result)){
-				
-				
-					echo "<div class='ui1'>";
-						echo "<form action='NewEN.php' method='get'>";
-							echo "<div class='ui1_box'>";
-								echo "<div class='ui1_box__inner'>";
-									echo "<h2>".$row['Header']."</h2><br>";
-									echo "<img class='newsandeventgalleryBox' src = 'name/".$row['Image']."> 
-									<div id='inner'>";
-										echo "<p>".$row['Detail']."</p>";
-										echo "<input type='hidden' name='IDnew' value='".$row['id']."'>";
-										echo "</div><br><br>";
-								
-										echo "<button type='submit' value='read'>Submit</button>";
-								
-								echo "</div>";
-							echo "</div>";
-						echo "</form>";
-					echo "</div>";
-						
-					
-				}
-			
-			?> 
+ <?php
+ 
+ require'./dbConnect.php';
+ $sql = "SELECT *FROM newth ORDER BY id DESC";
+ $result = mysqli_query($db, $sql);
+ 
+ while($row = mysqli_fetch_array($result)){
+  
+  
+   echo "<div class='ui1'>";
+   echo "<form action='NewTH.php' method='get'>";
+   echo "<div class='ui1_box'>";
+   echo "<div class='ui1_box__inner'>";
+   echo "<h2>".$row['Header']."</h2><br>";
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="JS/slider.js"></script>
-  <script src="JS/textBox.js"></script>
+   echo "<img class='newsandeventgalleryBox' src = 'name/".$row['Image']."' width='100px' height='100px;'>";
+
+   echo "<p>".$row['Detail']."<div id='inner'></p>";
+
+   echo "<button type='submit' value='read'>อ่านต่อ</button>";
+   echo "<input type='hidden' name='IDnew' value='".$row['id']."'>";
+
+   echo "</div><br><br>";
+
+   echo "</div>";
+   echo "</div>";
+   echo "</form>";
+   echo "</div>";
+   
+   
+ }
+ 
+ ?> 
+
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ <script src="JS/slider.js"></script>
+ <script src="JS/textBox.js"></script>
 </body>
 
 </html>
